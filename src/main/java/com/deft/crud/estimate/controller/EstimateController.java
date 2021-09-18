17654009c336c89fc789291649502d54bcee3a1d
deoksu Jo <antisnoopy88@gmail.com>
2021-09-19 05:06:54 +0900

    210918_deoksu_2 견적서 화면 구현 중

--- src/main/java/com/deft/crud/estimate/controller/EstimateController.java
+++ src/main/java/com/deft/crud/estimate/controller/EstimateController.java
@@ -1,5 +1,34 @@
 package com.deft.crud.estimate.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.deft.crud.estimate.model.dto.EstimateDTO;
+import com.deft.crud.estimate.model.service.EstimateService;
+
+@Controller
+@RequestMapping("/estimate")
 public class EstimateController {
+	
+	private final EstimateService estimateService;
+	
+	@Autowired
+	public EstimateController(EstimateService estimateService) {
+		this.estimateService = estimateService;
+	}
+	
+	
+	@GetMapping("/selectAll")
+	public ModelAndView selectEstimateSelectAll(ModelAndView mv) {
+		
+//		List<EstimateDTO> estimateList = estimateService.estimateSelectAll();
+		
+		return mv;
+	}
 
 }
--- src/main/java/com/deft/crud/main/model/service/MainService.java
+++
@@ -1,9 +0,0 @@
-package com.deft.crud.main.model.service;
-
-import java.util.List;
-
-public interface MainService {
-	
-	List<String> selectMenuList();
-
-}
