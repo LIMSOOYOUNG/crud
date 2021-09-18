e13c08710c56888408701b0eb02e3bbbed08f88f
deoksu Jo <antisnoopy88@gmail.com>
2021-09-18 23:17:47 +0900

    20210918_deoksu_2 merge 충돌 수정

--- src/main/java/com/deft/crud/main/controller/MainController.java
+++ src/main/java/com/deft/crud/main/controller/MainController.java
@@ -1,21 +1,11 @@
 package com.deft.crud.main.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.deft.crud.main.model.service.MainService;
-
 @Controller
 public class MainController {
-	
-	private MainService mainService;
-	
-	@Autowired
-	public MainController(MainService mainService) {
-		this.mainService = mainService;
-	}
 	
 	@GetMapping(value = {"/", "/main"})
 	public String main() {
@@ -24,12 +14,6 @@ public class MainController {
 	
 	@PostMapping(value = "/")
 	public String redirectMain() {
-		
-//		List<String> menuList = mainService.selectMenuList();
-//		
-//		for(String menu : menuList) {
-//			System.out.println(menu);
-//		}
 		
 		return "main/main";
 	}
