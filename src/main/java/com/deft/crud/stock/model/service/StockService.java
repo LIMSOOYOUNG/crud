034244557d4f6e2910b0662229cbfe86fb808d71
KBJ <KBJ@DESKTOP-DV387MS>
2021-09-19 15:37:49 +0900

    210919-stock컨트롤러 작성중

--- src/main/java/com/deft/crud/stock/model/service/StockService.java
+++ src/main/java/com/deft/crud/stock/model/service/StockService.java
@@ -1,5 +1,11 @@
 package com.deft.crud.stock.model.service;
 
+import java.util.List;
+
+import com.deft.crud.stock.model.dto.StockDTO;
+
 public interface StockService {
+
+	List<StockDTO> stockSelectAll();
 
 }
--- src/main/java/com/deft/crud/stock/model/service/StockServiceImpl.java
+++ src/main/java/com/deft/crud/stock/model/service/StockServiceImpl.java
@@ -1,5 +1,18 @@
 package com.deft.crud.stock.model.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.deft.crud.stock.model.dto.StockDTO;
+
+@Service
 public class StockServiceImpl implements StockService{
+
+	@Override
+	public List<StockDTO> stockSelectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
