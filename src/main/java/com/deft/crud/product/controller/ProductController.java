0349fa30f16a2ab9cd2ffb729519507fbc92a658
LIMSOOYOUNG <LIMSOOYOUNG@DESKTOP-5TI4AC4>
2021-09-28 10:58:15 +0900

    210927 임수영 상품 수정페이지 컨트롤러까지 구현 후 커밋

--- src/main/java/com/deft/crud/product/controller/ProductController.java
+++ src/main/java/com/deft/crud/product/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -68,6 +69,37 @@ public class ProductController {
 		mv.setViewName("product/productDetail");
 		
 		return mv;
+	}
+	
+	@PostMapping("/updateProduct") 
+	public ModelAndView upadateProduct(ModelAndView mv,
+									 	  @RequestParam("refCategoryName") String refCategoryName
+									 	, @RequestParam("categoryName")	String categoryName
+										, @RequestParam("companyName") String companyName
+										, @RequestParam("productNo") int productNo 
+										, @RequestParam("productName") String productName
+										, @RequestParam("purchasePrice") int purchasePrice
+										, @RequestParam("sellingPrice") int sellingPrice
+										, @RequestParam("sellStatus") String sellStatus) {
+		
+		System.out.println("=======");
+		System.out.println("=======");
+		System.out.println("=======");
+		System.out.println("=======");
+		
+		System.out.println(refCategoryName);
+		System.out.println(categoryName);
+		System.out.println(productNo);
+		System.out.println(productName);
+		System.out.println(purchasePrice);
+		System.out.println(sellingPrice);
+		System.out.println(sellStatus);
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		
+		return null;
 	}
 	
 	
