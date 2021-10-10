9421be95299ba5c51e9c222a187d3f09b06c2820
LIMSOOYOUNG <LIMSOOYOUNG@DESKTOP-5TI4AC4>
2021-10-10 11:32:19 +0900

    211009 ~ 211010 임수영 사원실적 조회

--- src/main/java/com/deft/crud/sales/controller/SalesController.java
+++ src/main/java/com/deft/crud/sales/controller/SalesController.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.deft.crud.member.model.service.UserImpl;
+import com.deft.crud.sales.model.dto.CollectBillDTO;
 import com.deft.crud.sales.model.dto.PerformanceDTO;
 import com.deft.crud.sales.model.dto.TargetPerfomDTO;
 import com.deft.crud.sales.model.service.SalesService;
@@ -42,10 +43,7 @@ public class SalesController {
 	public ModelAndView selectUserSalesAll(ModelAndView mv, @AuthenticationPrincipal UserImpl loginInfo) {
 		
 		int empNo = loginInfo.getEmpNo();
-		
-		/* 로그인 된 사원의 목표 실적 조회*/
-		List<TargetPerfomDTO> empTargetPerformList = salesService.empTargetPerformList(empNo);
-		
+
 		/* 폼에다 실적을 등록하기 위한 연과 월을 확인해주기 위해 LocalDate 생성 후 현재 연도와 월의 객체를 뷰에 전달*/
 		LocalDate insertTargetDate = LocalDate.now();
 		
@@ -53,6 +51,13 @@ public class SalesController {
 		String performMonth = Integer.toString(insertTargetDate.getMonthValue());       // 위와 내용 같음 (월)
 		System.out.println("monthValue : " + insertTargetDate.getMonthValue());
 		
+		TargetPerfomDTO targetPerformDate = new TargetPerfomDTO();
+		targetPerformDate.setPerformYear(performYear);
+		targetPerformDate.setPerformMonth(performMonth);
+		
+		/* 로그인 된 사원의 목표 실적 조회*/
+		List<TargetPerfomDTO> empTargetPerformList = salesService.empTargetPerformList(empNo, targetPerformDate);
+
 		System.out.println("empTargetPerformList : " + empTargetPerformList);
 		
 		mv.addObject("performYear", performYear);
@@ -65,82 +70,203 @@ public class SalesController {
 	
 	/* 사원의 실제 개인실적 조회를 ajax 통신으로 조회한 후 목표실적과 비교한다. */
 	@PostMapping("/user/perform/selectAll")
-	public ModelAndView selectUserPerformList(ModelAndView mv, HttpServletResponse response, @AuthenticationPrincipal UserImpl loginInfo) throws JsonProcessingException {
+	public ModelAndView selectUserPerformList(ModelAndView mv, HttpServletResponse response, 
+			@AuthenticationPrincipal UserImpl loginInfo, @RequestParam("performYear") String performYear) throws JsonProcessingException {
 		
 		response.setContentType("application/json; charset=UTF-8");
-
+		
+		/* 세션에 저장된 사원 정보를 가지고 온다. */
 		int empNo = loginInfo.getEmpNo();
 		
-		/* 사원 실제 월간 실적 조회 */
-		List<PerformanceDTO> empPerfomList = salesService.empPerformList(empNo);
-		System.out.println("empPerfomList : " + empPerfomList);
+		/* 사원 정보와 ajax 통신 할 때 받은 날짜 데이터와 세션에서 가져왔던 사원번호를 값을 담아준다. */
+		TargetPerfomDTO parameters = new TargetPerfomDTO();
+		parameters.setEmpNo(empNo);
+		parameters.setPerformYear(performYear);
 		
-		mv.addObject("empPerfomList", objectMapper.writeValueAsString(empPerfomList));
+		/* 사원 실제 월간 실적 조회 */
+		List <PerformanceDTO> empPerformList = salesService.empPerformList(parameters);
+		System.out.println("empPerfomList : " + empPerformList);
+		
+		mv.addObject("empPerformList", objectMapper.writeValueAsString(empPerformList));
 		mv.setViewName("jsonView");
 		return mv;
 	}
+
+	/* 사원의 실제 개인실적 조회를 ajax 통신으로 조회한 후 목표실적과 비교한다. */
+//	@PostMapping("/user/perform/selectAll")
+//	public ModelAndView selectUserPerformForYear(ModelAndView mv, HttpServletResponse response, 
+//			@AuthenticationPrincipal UserImpl loginInfo, @RequestParam("performYear") String performYear) throws JsonProcessingException {
+//		
+//		response.setContentType("application/json; charset=UTF-8");
+//		
+//		int empNo = loginInfo.getEmpNo();
+//		
+//		/* 사원 정보와 ajax 통신 할 때 받은 날짜 데이터와 세션에서 가져왔던 사원번호를 값을 담아준다. */
+//		TargetPerfomDTO parameters = new TargetPerfomDTO();
+//		parameters.setEmpNo(empNo);
+//		parameters.setPerformYear(performYear);
+//		
+//		/* 뷰에서 선택한 연도의 목표실적 조회*/
+//		List<TargetPerfomDTO> empTargetPerformList = salesService.empTargetPerformList(empNo, targetPerformDate);
+//
+//		/* 사원 실제 월간 실적 조회 */
+//		List <PerformanceDTO> empPerformList = salesService.empPerformList(parameters);
+//		System.out.println("empPerfomList : " + empPerformList);
+//		
+//		mv.addObject("empPerformList", objectMapper.writeValueAsString(empPerformList));
+//		mv.setViewName("jsonView");
+//		return mv;
+//	}
 	
 	/* 사원 개인 실적 상세조회*/
 	@GetMapping("/select")
-	public ModelAndView userPerformList(ModelAndView mv, 
-										@RequestParam("no") String performDate, @AuthenticationPrincipal UserImpl loginInfo) {
+	public ModelAndView selectUserPerformDetail(ModelAndView mv, @RequestParam("performNo") String performNo, 
+			@AuthenticationPrincipal UserImpl loginInfo) {
 		
 		int empNo = loginInfo.getEmpNo();
-		String performYear = performDate.substring(0, 4);
-		String performMonth = performDate.substring(4);
+		String collectBillYear = performNo.substring(0, 4);
+		String collectBillMonth = performNo.substring(4);
 		
-		System.out.println("performYear : " + performYear);
-		System.out.println("performMonth : " + performMonth);
+		System.out.println("collectBillYear : " + collectBillYear);
+		System.out.println("collectBillMonth : " + collectBillMonth);
 		
-		System.out.println("performDate : " + performDate);
+		System.out.println("performNo : " + performNo);
 		
-		List<PerformanceDTO> selectUserPerformDetail = salesService.selectUserPerformDetail(empNo, performYear, performMonth);
+		CollectBillDTO collectBillDate = new CollectBillDTO();
+		collectBillDate.setCollectBillYear(collectBillYear);
+		collectBillDate.setCollectBillMonth(collectBillMonth);
+		
+		
+		List<PerformanceDTO> selectUserPerformDetail = salesService.selectUserPerformDetail(empNo, collectBillDate);
 		
 		System.out.println("selectUserPerformDetail : " + selectUserPerformDetail);
 		
-		mv.addObject("performYear", performYear);
-		mv.addObject("performMonth", performMonth);
+		mv.addObject("collectBillYear", collectBillYear);
+		mv.addObject("collectBillMonth", collectBillMonth);
 		mv.addObject("selectUserPerformDetail", selectUserPerformDetail);
 		mv.setViewName("sales/selectEmpSalesDetail");
 		return mv;
 	}
 	
 	
-	/* 부서별 영업실적*/
+	/* 부서 전체 영업실적*/
 	@GetMapping("/dept/selectAll")
-	public ModelAndView deptPerformList(ModelAndView mv) {
+	public ModelAndView allDeptPerformList(ModelAndView mv) {
 		
-		List<PerformanceDTO> selectDeptPerformList = salesService.selectDeptPerformList();
+		LocalDate deptPerformDate = LocalDate.now();
+
+		String collectBillYear = Integer.toString(deptPerformDate.getYear());				// 디비에 값 저장을 위해 String 타입으로 형변환(연도)
+		String collectBillMonth = Integer.toString(deptPerformDate.getMonthValue());        // 위와 내용 같음 (월)
+		
+		CollectBillDTO collectBillDate = new CollectBillDTO();
+		collectBillDate.setCollectBillYear(collectBillYear);
+		collectBillDate.setCollectBillMonth(collectBillMonth);
+		
+		System.out.println(collectBillDate.getCollectBillYear());
+		System.out.println(collectBillDate.getCollectBillMonth());
+		
+		List<PerformanceDTO> selectDeptPerformList = salesService.selectDeptPerformList(collectBillDate);
 		
 		System.out.println("selectDeptPerformList : " + selectDeptPerformList);
 		
+		/* 연도와 월을 비교하기 위해 */
 		mv.addObject("selectDeptPerformList", selectDeptPerformList);
+		mv.addObject("collectBillYear", collectBillYear);
+		mv.addObject("collectBillMonth", collectBillMonth);
 		mv.setViewName("sales/selectDeptSales");
+		return mv;
+		
+	}
+	
+	/* ajax 통신으로 폼에서 입력한 연도와 월에 대한 부서 실적 정보를 가지고 온다.*/
+	@GetMapping("/select/perform/date")
+	public ModelAndView selectPerformForDate(ModelAndView mv, HttpServletResponse response,
+			@ModelAttribute CollectBillDTO parameters) throws JsonProcessingException {
+		
+		response.setContentType("application/json; charset=UTF-8");
+		
+		System.out.println("parameters : " + parameters);
+		
+		List<PerformanceDTO> selectPerformForDate = salesService.selectPerformForDate(parameters);
+		
+		System.out.println("selectPerformForDate : " + selectPerformForDate);
+		
+		mv.addObject("selectPerformForDate", objectMapper.writeValueAsString(selectPerformForDate));
+		mv.setViewName("jsonView");
+		return mv;
+		
+	}
+	
+	/* 한 부서에 대한 사원들의 영업실적 조회*/
+	@GetMapping("/dept/select")
+	public ModelAndView deptEachPerformList(ModelAndView mv, @RequestParam String deptCode,
+			@ModelAttribute CollectBillDTO parameters) {
+		
+		System.out.println("parameters : " + parameters);
+		System.out.println("deptCode : " + deptCode);
+		
+		/* 첫번째 보여질 페이지는 사원들의 현재 연도의 월에 대한 실적 정보여서 
+		 * 현재 시간기준으로 연도와 월을 포맷한 후 인자값으로 넘긴다.*/
+		List<PerformanceDTO> selectDeptEachPerformList = salesService.selectDeptEachPerformList(parameters, deptCode);
+		
+		mv.addObject("parameters", parameters);
+		mv.addObject("selectDeptEachPerformList", selectDeptEachPerformList);
+		mv.setViewName("sales/selectDeptEachSales");
 		return mv;
 	}
 	
-	/* 사원 목표 실적 등록*/
-	@PostMapping("/insert/target")
-	public ModelAndView insertTargetSales(ModelAndView mv, @AuthenticationPrincipal UserImpl loginInfo, 
-			@ModelAttribute TargetPerfomDTO parameters, RedirectAttributes rttr) {
+	/* 한 부서에 대한 사원들의 연도와 월에 맞게 목표실적을 조회한다. */
+	@PostMapping("/dept/target/perform/selectAll")
+	public ModelAndView deptTargetPerformList(ModelAndView mv, HttpServletResponse response,
+			@ModelAttribute CollectBillDTO parameters) throws JsonProcessingException {
 		
-		int empNo = loginInfo.getEmpNo();
 		
-		parameters.setEmpNo(empNo);
+		response.setContentType("application/json; charset=UTF-8");
 		
-		int result = salesService.insertTargetSales(parameters, loginInfo);
+		List<TargetPerfomDTO> deptTargetPerformList = salesService.deptTargetPerformList(parameters);
 		
-		String message = "";
+		System.out.println("deptTargetPerformList : " + deptTargetPerformList );
+		System.out.println("parameters : " + parameters);
 		
-		if(result > 0) {
-			message = "목표 실적 등록을 성공하셨습니다!";
-		} else {
-			message = "이미 실적을 등록하셨습니다!";
-		}
-		
-		rttr.addFlashAttribute("message", message);
-		mv.setViewName("redirect:/sales/selectAll");
+		mv.addObject("deptTargetPerformList", objectMapper.writeValueAsString(deptTargetPerformList));
+		mv.setViewName("jsonView");
 		return mv;
 	}
+	
+	/* 한 부서의 사원실적 상세 조회 */
+//	@GetMapping("/dept/emp/select")
+//	public ModelAndView selectDeptUserPerformDetail(ModelAndView mv, @RequestParam String empNo,
+//			@ModelAttribute CollectBillDTO parameters) {
+//		
+//		
+//		System.out.println(empNo);
+//		System.out.println(parameters);
+//		
+//		return null;
+//	}
+	
+	/* 사원 목표 실적 등록*/
+//	@PostMapping("/insert/target")
+//	public ModelAndView insertTargetSales(ModelAndView mv, @AuthenticationPrincipal UserImpl loginInfo, 
+//			@ModelAttribute TargetPerfomDTO parameters, RedirectAttributes rttr) {
+//		
+//		int empNo = loginInfo.getEmpNo();
+//		
+//		parameters.setEmpNo(empNo);
+//		
+//		int result = salesService.insertTargetSales(parameters, loginInfo);
+//		
+//		String message = "";
+//		
+//		if(result > 0) {
+//			message = "목표 실적 등록을 성공하셨습니다!";
+//		} else {
+//			message = "이미 실적을 등록하셨습니다!";
+//		}
+//		
+//		rttr.addFlashAttribute("message", message);
+//		mv.setViewName("redirect:/sales/selectAll");
+//		return mv;
+//	}
 	
 }
--- src/main/java/com/deft/crud/sales/model/service/SalesService.java
+++ src/main/java/com/deft/crud/sales/model/service/SalesService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.deft.crud.member.model.service.UserImpl;
 import com.deft.crud.sales.model.dao.SalesMapper;
+import com.deft.crud.sales.model.dto.CollectBillDTO;
 import com.deft.crud.sales.model.dto.PerformanceDTO;
 import com.deft.crud.sales.model.dto.TargetPerfomDTO;
 
@@ -22,59 +23,76 @@ public class SalesService {
 		this.salesMapper = salesMapper;
 	}
 	
-	public List<TargetPerfomDTO> empTargetPerformList(int empNo) {
+	public List<TargetPerfomDTO> empTargetPerformList(int empNo, TargetPerfomDTO targetPerformDate) {
 		
 		
-		return salesMapper.empTargetPerformList(empNo);
+		return salesMapper.empTargetPerformList(empNo, targetPerformDate);
 	}
 
-	public List<PerformanceDTO> empPerformList(int empNo) {
+	public List<PerformanceDTO> empPerformList(TargetPerfomDTO parameters) {
 
-		return salesMapper.empPerformList(empNo);
+		return salesMapper.empPerformList(parameters);
 	}
 
-	@Transactional
-	public int insertTargetSales(TargetPerfomDTO parameters, UserImpl loginInfo) {
-		
-		
-		/* 한 사원의 목표 실적을 조회하기 위해 전체 목표 실적을 가지고 온 후
-		 * 해당 사원이 이미 현재 연도와 월에 입력한 목표 실적을 등록을 했다면 
-		 * 해당 월에 중복으로 등록을 못하게 로직 처리를 한다.*/
-		int empNo = loginInfo.getEmpNo();
-		
-		System.out.println("empNo : " + empNo);
-		
-		int result = 0;
-		boolean isSaved = false; 
-		
-		List<TargetPerfomDTO> empTargetPerformList = salesMapper.empTargetPerformList(empNo);
-		
-		for(int i = 0; i < empTargetPerformList.size(); i++) {
-			if(parameters.getPerformYear().equals(empTargetPerformList.get(i).getPerformYear()) && parameters.getPerformMonth().equals(empTargetPerformList.get(i).getPerformMonth())) {
-				isSaved = true;
-			}
-		}
-		
-		if(!isSaved) {
-			result = salesMapper.insertTargetSales(parameters);
-		}
-		
-		System.out.println("parameters : " + parameters);
-		return result;
-		
-	}
+//	@Transactional
+//	public int insertTargetSales(TargetPerfomDTO parameters, UserImpl loginInfo) {
+//		
+//		
+//		/* 한 사원의 목표 실적을 조회하기 위해 전체 목표 실적을 가지고 온 후
+//		 * 해당 사원이 이미 현재 연도와 월에 입력한 목표 실적을 등록을 했다면 
+//		 * 해당 월에 중복으로 등록을 못하게 로직 처리를 한다.*/
+//		int empNo = loginInfo.getEmpNo();
+//		
+//		System.out.println("empNo : " + empNo);
+//		
+//		int result = 0;
+//		boolean isSaved = false; 
+//		
+//		List<TargetPerfomDTO> empTargetPerformList = salesMapper.empTargetPerformList(empNo);
+//		
+//		for(int i = 0; i < empTargetPerformList.size(); i++) {
+//			if(parameters.getPerformYear().equals(empTargetPerformList.get(i).getPerformYear()) && parameters.getPerformMonth().equals(empTargetPerformList.get(i).getPerformMonth())) {
+//				isSaved = true;
+//			}
+//		}
+//		
+//		if(!isSaved) {
+//			result = salesMapper.insertTargetSales(parameters);
+//		}
+//		
+//		System.out.println("parameters : " + parameters);
+//		return result;
+//		
+//	}
 
 	/* 개인 실적 상세조회 */
-	public List<PerformanceDTO> selectUserPerformDetail(int empNo, String performYear, String performMonth) {
+	public List<PerformanceDTO> selectUserPerformDetail(int empNo, CollectBillDTO collectBillDate) {
 		
-		return salesMapper.selectUserPerformDetail(empNo, performYear, performMonth);
+		return salesMapper.selectUserPerformDetail(empNo, collectBillDate);
 	}
 	
 	
 	/* 부서 실적 조회*/
-	public List<PerformanceDTO> selectDeptPerformList() {
+	public List<PerformanceDTO> selectDeptPerformList(CollectBillDTO collectBillDate) {
 
-		return salesMapper.selectDeptPerformList();
+		return salesMapper.selectDeptPerformList(collectBillDate);
+	}
+	
+	
+	/* 부서별 사원 실적 전체 조회*/
+	public List<PerformanceDTO> selectDeptEachPerformList(CollectBillDTO parameters, String deptCode) {
+		
+		return salesMapper.selectDeptEachPerformList(parameters, deptCode);
+	}
+
+	public List<PerformanceDTO> selectPerformForDate(CollectBillDTO parameters) {
+		
+		return salesMapper.selectPerformForDate(parameters);
+	}
+
+	public List<TargetPerfomDTO> deptTargetPerformList(CollectBillDTO parameters) {
+
+		return salesMapper.deptTargetPerformList(parameters);
 	}
 
 	
